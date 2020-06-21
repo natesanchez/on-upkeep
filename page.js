cca7a08df2e4ea5498a4be2e5f7fe2f1269429e0
Nate_Sanchez <NSanchez@usa.com>
2020-06-21 10:36:41 -0400

    #1

---
+++ page.js
@@ -0,0 +1,7 @@
+/* 1.) Sort the array in reverse order. (Not alphabetical, but reverse
+ the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])*/
+
+//let testArr = ["a", "c", "b"];
+//testArr.reverse();
+
+/* 2.) */
